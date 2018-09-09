23176714a6ce93099c982c065b140824a40fa341
pshabbaki <pshabbaki24@gmail.com>
2018-09-08 20:54:47 -0500

    debugging

--- test_cases.py
+++ test_cases.py
@@ -15,12 +15,11 @@ class TestStringMethods(unittest.TestCase):
         self.assertTrue(s.get("http://shufflecards.azurewebsites.net/deck/pop").text == '{"deck":[],"dealt":[]}')
   
  
-#     def test_post_empty(self):
-#         s = requests.Session()
-#         s_json = []
-#         print s.post("http://shufflecards.azurewebsites.net/deck", json=s_json).text
-#         self.assertTrue(s.post("http://shufflecards.azurewebsites.net/deck", json=s_json).text == '{"deck":[]}')
-#         self.assertTrue(s.get("http://shufflecards.azurewebsites.net/deck").text, '{"deck":[]}')
+    def test_post_empty(self):
+        s = requests.Session()
+        s_json = []
+        self.assertTrue(s.post("http://shufflecards.azurewebsites.net/deck", json=s_json).status_code == 400)
+        self.assertTrue(s.get("http://shufflecards.azurewebsites.net/deck").text, '{"deck":[]}')
   
     def test_post_one_session(self):
         s = requests.Session()
@@ -29,20 +28,21 @@ class TestStringMethods(unittest.TestCase):
         s_get = json.loads(s.get("http://shufflecards.azurewebsites.net/deck").text)
         self.assertTrue(s_get['deck'] == s_json)
         self.assertTrue('dealt' not in s_get['deck'])        
-        
-
+         
+ 
 #      
 #      
-#     def test_shuffle_session(self):
-#         s = requests.Session()
-#         s_json = [1,2,3,4,5,6,7]
+    def test_shuffle_session(self):
+        s = requests.Session()
+        s_json = [1,2,3,4,5,6,7]
 # 
 # 
-#         self.assertTrue(s.post("http://shufflecards.azurewebsites.net/deck", json=s_json).text == '{"deck":[1,2,3,4,5,6,7]}')
-#         self.assertTrue(s.get("http://shufflecards.azurewebsites.net/deck").text == '{"deck":[1,2,3,4,5,6,7]}')
-#         #the length should still be the same
-#         self.assertTrue(len(s.get("http://shufflecards.azurewebsites.net/deck/shuffle").text) == len('[1,2,3,4,5,6,7]'),  msg="should not throw an error")
-     
+        self.assertTrue(s.post("http://shufflecards.azurewebsites.net/deck", json=s_json).text == '{"deck":[1,2,3,4,5,6,7]}')
+        self.assertTrue(s.get("http://shufflecards.azurewebsites.net/deck").text == '{"deck":[1,2,3,4,5,6,7]}')
+        #the length should still be the same
+        json_shuffle = json.loads(s.get("http://shufflecards.azurewebsites.net/deck/shuffle").text)
+        self.assertTrue(len(json_shuffle['deck']) == len(s_json),  msg="should not throw an error")
+       
     def test_post_two_session(self):
         s = requests.Session()
         k = requests.Session()
@@ -50,14 +50,14 @@ class TestStringMethods(unittest.TestCase):
         k_json = [8,9,10,11,12,13,14]
         s.post("http://shufflecards.azurewebsites.net/deck", json=s_json).text
         k.post("http://shufflecards.azurewebsites.net/deck", json=k_json).text
-  
+   
         self.assertTrue(s.get("http://shufflecards.azurewebsites.net/deck").text == '{"deck":[1,2,3,4,5,6,7]}')
         self.assertTrue(k.get("http://shufflecards.azurewebsites.net/deck").text == '{"deck":[8,9,10,11,12,13,14]}')
     #     #the length should still be the same
         self.assertTrue(len(s.get("http://shufflecards.azurewebsites.net/deck/shuffle").text) == len('{"deck":[1,2,3,4,5,6,7]}'),  msg="should not throw an error")
         self.assertTrue(len(k.get("http://shufflecards.azurewebsites.net/deck/shuffle").text) == len('{"deck":[10,13,9,14,12,8,11]}'), msg="should not throw an error")
- 
- 
+  
+  
     def test_whole_flow(self):
         s = requests.Session()
         k = requests.Session()
@@ -65,48 +65,48 @@ class TestStringMethods(unittest.TestCase):
         k_json = [8,9,10,11,12,13,14]
         s.post("http://shufflecards.azurewebsites.net/deck", json=s_json)
         k.post("http://shufflecards.azurewebsites.net/deck", json=k_json)
-        
+         
         self.assertTrue(s.get("http://shufflecards.azurewebsites.net/deck").text == '{"deck":[1,2,3,4,5,6,7]}')
         self.assertTrue(k.get("http://shufflecards.azurewebsites.net/deck").text == '{"deck":[8,9,10,11,12,13,14]}')
-         
+          
     #     #the length should still be the same
         self.assertTrue(len(s.get("http://shufflecards.azurewebsites.net/deck/shuffle").text) == len('{"deck":[1,2,3,4,5,6,7]}'),  msg="should not throw an error")
         self.assertTrue(len(k.get("http://shufflecards.azurewebsites.net/deck/shuffle").text) == len('{"deck":[10,13,9,14,12,8,11]}'), msg="should not throw an error")
-        
+         
         s_pop = json.loads(s.get("http://shufflecards.azurewebsites.net/deck/pop").text)
         k_pop = json.loads(k.get("http://shufflecards.azurewebsites.net/deck/pop").text)
     #     #the length should still be one less
         self.assertTrue(len(s_pop['dealt'])>0, msg="should not throw an error")
         self.assertTrue(len(k_pop['dealt'])>0, msg="should not throw an error")
-    
+     
 #         
     def test_whole_flow_one_session(self):
         s = requests.Session()
         s_json = [1,2,3,4]
         s.post("http://shufflecards.azurewebsites.net/deck", json=s_json)
         self.assertTrue(s.get("http://shufflecards.azurewebsites.net/deck").text == '{"deck":[1,2,3,4]}')
-        
+         
         #first pop
         s_pop = json.loads(s.get("http://shufflecards.azurewebsites.net/deck/pop").text)
         s_get = json.loads(s.get("http://shufflecards.azurewebsites.net/deck").text)
         self.assertTrue(len(s_pop['dealt']) == len(s_get['dealt']) > 0,  msg="should not throw an error")
-        
+         
         s_get = json.loads(s.get("http://shufflecards.azurewebsites.net/deck").text)
         self.assertTrue(len(s_get['deck']) == len(s_json)-1)
         self.assertTrue(len(s_get['dealt']) == 1)
-
+ 
     #     #second pop
         s_pop = json.loads(s.get("http://shufflecards.azurewebsites.net/deck/pop").text)
         s_get = json.loads(s.get("http://shufflecards.azurewebsites.net/deck").text)
         self.assertTrue(len(s_pop['dealt']) == len(s_get['dealt']) == 2,  msg="should not throw an error")
-        
- 
+         
+  
     #     #third pop
         s_pop = json.loads(s.get("http://shufflecards.azurewebsites.net/deck/pop").text)
         s_get = json.loads(s.get("http://shufflecards.azurewebsites.net/deck").text)
         self.assertTrue(len(s_pop['dealt']) == len(s_get['dealt']) == 3,  msg="should not throw an error")
-        
- 
+         
+  
     #     #last pop -- no deck, only dealt
         s_pop = json.loads(s.get("http://shufflecards.azurewebsites.net/deck/pop").text)
         s_get = json.loads(s.get("http://shufflecards.azurewebsites.net/deck").text)
